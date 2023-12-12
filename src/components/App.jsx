eaeba18bbc3daf8ad90f2af2423b18d78404d48e
ElenaSutkova <134842923+ElenaSutkova@users.noreply.github.com>
2023-12-12 15:24:21 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -50,30 +50,5 @@ class App extends Component {
   }
 }
 
-// const App = () => {
-//   const [state, setState] = useState({
-//     good: 0,
-//     neutral: 0,
-//     bad: 0
-//   });
-
-//   const handleFeedback = (type) => {
-//     setState((prev) => ({ ...prev, [type]: prev[type] + 1 }));
-//   }
-//   return (
-//     <div>
-//       <Section title="Please leave feedback">
-//         <Feedback options={['good', 'neutral', 'bad']} onLeaveFeedback={handleFeedback}/>
-//       </Section>
-//       {state.good + state.neutral + state.bad > 0 ? (
-//         <Section title="Statistics">
-//           <Statistic good={state.good} neutral={state.neutral} bad={state.bad} />
-//         </Section>
-//       ) : (
-//           <p>No feedback given</p>
-//       )}
-//     </div>
-//   )
-// }
 
 export default App;
